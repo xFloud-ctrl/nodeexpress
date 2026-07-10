f847709ff0dade4bd365ae0af6a7273ecbe3fd2f
xFloud-ctrl <muhagames91@gmail.com>
2026-07-10 22:33:54 +0500

    Fix database connection URI

--- app.mjs
+++ app.mjs
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 const port = process.env.PORT || 3001;
 // database connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => console.log('Connected to MongoDB'))
  
   .catch((err) => console.log(err));
