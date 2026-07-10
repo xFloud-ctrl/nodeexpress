3ca1b7ac23881eca3a00ac40f9d3175fb42da2bd
xFloud-ctrl <muhagames91@gmail.com>
2026-07-10 19:20:06 +0500

    Fix undefined port variable

--- app.mjs
+++ app.mjs
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // view engine
 app.set('view engine', 'ejs');
-const port = process.env.PORT || 3001;
+
 // database connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => console.log('Connected to MongoDB'))
